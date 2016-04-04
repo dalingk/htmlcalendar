e1b88fcabbe332b90e15e045149b20606484d1d8
Kyle Daling <kyle-daling@live.com>
2016-04-03 19:07:55 -0700

    Added ability to fullscreen calendar
    Enable by changing calendar.enableFullScreen to true

--- calendar.js
+++ calendar.js
@@ -1,8 +1,26 @@
 "use strict";
 var calendar = {
     days: {},
-    fullscreen: function () {
-        return false;
+    enableFullScreen: false,
+    isFullscreen: false,
+    icons:  {
+        fullscreen: "<svg fill=\"#000000\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z\"/></svg>", // https://design.google.com/icons/#ic_fullscreen
+        fullscreenExit: "<svg fill=\"#000000\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z\"/></svg>" // https://design.google.com/icons/#ic_fullscreen
+    },
+    fullscreen: function (e, calendarID) {
+        var node = e.target;
+        while (!node.classList.contains("pad")) {
+            node = node.parentNode;
+        }
+        if (calendar.isFullscreen) {
+            calendar.isFullscreen = false;
+            node.innerHTML = calendar.icons.fullscreen;
+            document.getElementById(calendarID).classList.remove("fullscreen");
+        } else {
+            calendar.isFullscreen = true;
+            node.innerHTML = calendar.icons.fullscreenExit;
+            document.getElementById(calendarID).classList.add("fullscreen");
+        }
     },
     maximizeDay: function (e) {
         e.preventDefault();
@@ -19,9 +37,18 @@ var calendar = {
         }
     },
     init: function(calendarID) {
-        this.days = document.getElementById(calendarID).getElementsByClassName("day");
+        var c = document.getElementById(calendarID);
+        this.days = c.getElementsByClassName("day");
         for (var i = 0; i < this.days.length; i++) {
             this.days[i].addEventListener("click", function (e) {calendar.maximizeDay(e)}, false);
+        }
+        if (calendar.enableFullScreen) {
+            var pad = c.getElementsByClassName("pad");
+            for (var i = 0; i < pad.length; i++) {
+                pad[i].addEventListener("click", function (e) {calendar.fullscreen(e, calendarID)}, false);
+                pad[i].innerHTML = calendar.icons.fullscreen;
+                pad[i].style.cursor = "pointer";
+            }
         }
     }
 }
